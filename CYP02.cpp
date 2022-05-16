c6c026cd56b11481aaf48d414d7abbaa85093d82
Miguel Contreras <79333663+iMike-505@users.noreply.github.com>
2022-05-15 23:29:59 -0500

    Add files via upload

---
+++ CYP02.cpp
@@ -0,0 +1,18 @@
+// CYP02.cpp : Este archivo contiene la función "main". La ejecución del programa comienza y termina ahí.
+//
+
+#include <iostream>
+
+int main()
+{
+	int bfSize;
+	int Pixel;
+
+		scanf_s("%i %i", &bfSize, &Pixel);
+
+		printf("BM%08x%016x", bfSize, Pixel);
+		return 0;
+}
+
+/*La variable bfize es el tmaño de la imagen y 
+la variable Pixel es la poscicion del pixel*/
